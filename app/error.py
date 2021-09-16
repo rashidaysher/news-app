6973a2c50964f596ad976b2a1bc8ece5b5f4e358
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-09-16 15:27:21 +0300

    a function to get the 404 page

--- app/error.py
+++ app/error.py
@@ -4,6 +4,6 @@ from app import app
 @app.errorhandler(404)
 def four_Ow_four(error):
     '''
-    Function to render the 404 error page
+    An eeror page to render the function of 404
     '''
     return render_template('fourOwfour.html'),404
